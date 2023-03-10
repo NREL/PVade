73315080affcba03338f2bf8b49834d503fd2772
Young <eyoung55@gmail.com>
2023-03-10 14:51:05 -0700

    writing simple tests for Parameters

--- pvade/tests/test_Parameters.py
+++ pvade/tests/test_Parameters.py
@@ -1,15 +1,62 @@
 import pytest
+import os
+
 from pvade.Parameters import SimParams
+
+filename = "pvade/tests/test_inputs.yaml"
+
 
 @pytest.fixture()
 def create_params():
-    params = SimParams("test_params.yaml")
+    # Create the file
+    with open(filename, "w") as fp:
+        fp.write("general:\n")
+        fp.write("  example: test_name\n")
+        fp.write("  output_dir_sol: path/to/solution/dir\n")
+        fp.write("domain:\n")
+        fp.write("  x_min: 0.0\n")
+        fp.write("  x_max: 2.5\n")
+        fp.write("solver:\n")
+        fp.write("  t_final: 1.0\n")
+        fp.write("  dt: 0.1\n")
+        fp.write("  save_xdmf_interval: 0.5\n")
+        fp.write("  save_text_interval: 0.5\n")
 
-    return params
+    # Run the actual test
+    yield
+
+    # Teardown
+    os.remove(filename)
+
+
+@pytest.fixture()
+def create_bad_params():
+    # Create the file
+    with open(filename, "w") as fp:
+        fp.write("generalTYPO:\n")
+        fp.write("  example: test_name\n")
+        fp.write("  output_dir_sol: path/to/solution/dir\n")
+
+    # Run the actual test
+    yield
+
+    # Teardown
+    os.remove(filename)
+
 
 @pytest.mark.unit
 def test_parameters(create_params):
-	params = create_params
-	print(params)
 
-	print('yo')
+    params = SimParams(filename)
+
+    assert params.general.output_dir_sol == "path/to/solution/dir"
+
+    assert params.domain.x_min == pytest.approx(0.0)
+
+    assert params.solver.t_steps == 10
+    assert params.solver.save_xdmf_interval_n == 5
+
+
+def test_bad_parameters(create_bad_params):
+    with pytest.raises(Exception):
+        params = SimParams(filename)
