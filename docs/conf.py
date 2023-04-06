ee7604b56692ccbfcf1823fc5ecbad7ac5098be2
Young <eyoung55@gmail.com>
2023-04-05 19:55:44 -0600

    the black format test works! formatting 6 files as it suggests

--- docs/conf.py
+++ docs/conf.py
@@ -12,14 +12,15 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../pvade'))
+
+sys.path.insert(0, os.path.abspath("../pvade"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PVade'
-copyright = '2023, Walid Arsalane, Ethan Young'
-author = 'Walid Arsalane, Ethan Young'
+project = "PVade"
+copyright = "2023, Walid Arsalane, Ethan Young"
+author = "Walid Arsalane, Ethan Young"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,18 +28,19 @@ author = 'Walid Arsalane, Ethan Young'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.napoleon',
-              'sphinx.ext.autosectionlabel'
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosectionlabel",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,11 +49,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-autoclass_content = 'both'
+autoclass_content = "both"
