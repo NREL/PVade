beeb984ec7cc492c5875542fe88d1411f9eede49
Melissa Rasmussen <melissa.rasmussen@nrel.gov>
2024-06-17 09:50:01 -0400

    Copy over Cylinder Flow example

---
+++ examples/cylinderflow.py
@@ -0,0 +1,364 @@
+import gmsh
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import tqdm.autonotebook
+from pathlib import Path
+
+from mpi4py import MPI
+from petsc4py import PETSc
+
+from basix.ufl import element
+
+from dolfinx.cpp.mesh import to_type, cell_entity_type
+from dolfinx.fem import (Constant, Function, functionspace,
+                         assemble_scalar, dirichletbc, form, locate_dofs_topological, set_bc)
+from dolfinx.fem.petsc import (apply_lifting, assemble_matrix, assemble_vector,
+                               create_vector, create_matrix, set_bc)
+from dolfinx.graph import adjacencylist
+from dolfinx.geometry import bb_tree, compute_collisions_points, compute_colliding_cells
+from dolfinx.io import (VTXWriter, distribute_entity_data, gmshio, XDMFFile)
+from dolfinx.mesh import create_mesh, meshtags_from_entities, locate_entities_boundary
+from ufl import (FacetNormal, Identity, Measure, TestFunction, TrialFunction,
+                 as_vector, div, dot, ds, dx, inner, lhs, grad, nabla_grad, rhs, sym, system)
+
+
+gmsh.initialize()
+
+L = 2.2
+H = 0.41
+c_x = c_y = 0.2
+r = 0.05
+gdim = 2
+mesh_comm = MPI.COMM_WORLD
+model_rank = 0
+if mesh_comm.rank == model_rank:
+    rectangle = gmsh.model.occ.addRectangle(0, 0, 0, L, H, tag=1)
+    obstacle = gmsh.model.occ.addDisk(c_x, c_y, 0, r, r)
+    fluid = gmsh.model.occ.cut([(gdim, rectangle)], [(gdim, obstacle)])
+    gmsh.model.occ.synchronize()
+
+# add markers
+fluid_marker = 1
+obstacle_marker = 2
+inlet_marker, outlet_marker, wall_marker, edge_marker = 3, 4, 5, 6
+inflow, outflow, walls, edge = [], [], [], []
+
+if mesh_comm.rank == model_rank:
+    volumes = gmsh.model.getEntities(dim=gdim)
+    assert (len(volumes) == 1)
+    gmsh.model.addPhysicalGroup(volumes[0][0], [volumes[0][1]], fluid_marker)
+    gmsh.model.setPhysicalName(volumes[0][0], fluid_marker, "Fluid")
+    #gmsh.model.addPhysicalGroup(volumes[1][0], [volumes[1][1]], obstacle_marker)
+    #gmsh.model.setPhysicalName(volumes[1][0], obstacle_marker, "Obstacle")
+
+
+if mesh_comm.rank == model_rank:
+    boundaries = gmsh.model.getBoundary(volumes, oriented=False)
+    for boundary in boundaries:
+        center_of_mass = gmsh.model.occ.getCenterOfMass(boundary[0], boundary[1])
+        if np.allclose(center_of_mass, [0, H / 2, 0]):
+            inflow.append(boundary[1])
+        elif np.allclose(center_of_mass, [L, H / 2, 0]):
+            outflow.append(boundary[1])
+        elif np.allclose(center_of_mass, [L / 2, H, 0]) or np.allclose(center_of_mass, [L / 2, 0, 0]):
+            walls.append(boundary[1])
+        else:
+            edge.append(boundary[1])
+    gmsh.model.addPhysicalGroup(1, walls, wall_marker)
+    gmsh.model.setPhysicalName(1, wall_marker, "Walls")
+    gmsh.model.addPhysicalGroup(1, inflow, inlet_marker)
+    gmsh.model.setPhysicalName(1, inlet_marker, "Inlet")
+    gmsh.model.addPhysicalGroup(1, outflow, outlet_marker)
+    gmsh.model.setPhysicalName(1, outlet_marker, "Outlet")
+    gmsh.model.addPhysicalGroup(1, edge, edge_marker)
+    gmsh.model.setPhysicalName(1, edge_marker, "Obstacle Edge")
+
+# Create distance field from obstacle.
+# Add threshold of mesh sizes based on the distance field
+# LcMax -                  /--------
+#                      /
+# LcMin -o---------/
+#        |         |       |
+#       Point    DistMin DistMax
+res_min = r / 3
+if mesh_comm.rank == model_rank:
+    distance_field = gmsh.model.mesh.field.add("Distance")
+    gmsh.model.mesh.field.setNumbers(distance_field, "EdgesList", edge)
+    threshold_field = gmsh.model.mesh.field.add("Threshold")
+    gmsh.model.mesh.field.setNumber(threshold_field, "IField", distance_field)
+    gmsh.model.mesh.field.setNumber(threshold_field, "LcMin", res_min)
+    gmsh.model.mesh.field.setNumber(threshold_field, "LcMax", 0.25 * H)
+    gmsh.model.mesh.field.setNumber(threshold_field, "DistMin", r)
+    gmsh.model.mesh.field.setNumber(threshold_field, "DistMax", 2 * H)
+    min_field = gmsh.model.mesh.field.add("Min")
+    gmsh.model.mesh.field.setNumbers(min_field, "FieldsList", [threshold_field])
+    gmsh.model.mesh.field.setAsBackgroundMesh(min_field)
+
+# generate the mesh
+if mesh_comm.rank == model_rank:
+    gmsh.option.setNumber("Mesh.Algorithm", 8)
+    gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 2)
+    gmsh.option.setNumber("Mesh.RecombineAll", 1)
+    gmsh.option.setNumber("Mesh.SubdivisionAlgorithm", 1)
+    gmsh.model.mesh.generate(gdim)
+    gmsh.model.mesh.setOrder(2)
+    gmsh.model.mesh.optimize("Netgen")
+
+mesh, _, ft = gmshio.model_to_mesh(gmsh.model, mesh_comm, model_rank, gdim=gdim)
+ft.name = "Facet markers"
+
+# problem specific parameters
+t = 0
+T = 1 / 25                      # Final time
+dt = 1 / 250                 # Time step size
+num_steps = int(T / dt)
+k = Constant(mesh, PETSc.ScalarType(dt))
+mu = Constant(mesh, PETSc.ScalarType(0.001))  # Dynamic viscosity
+rho = Constant(mesh, PETSc.ScalarType(1))     # Density
+
+# boundary conditions
+v_cg2 = element("Lagrange", mesh.topology.cell_name(), 2, shape=(mesh.geometry.dim, ))
+s_cg1 = element("Lagrange", mesh.topology.cell_name(), 1)
+V = functionspace(mesh, v_cg2)
+Q = functionspace(mesh, s_cg1)
+
+fdim = mesh.topology.dim - 1
+
+class InletVelocity():
+    def __init__(self, t):
+        self.t = t
+
+    def __call__(self, x):
+        values = np.zeros((gdim, x.shape[1]), dtype=PETSc.ScalarType)
+        values[0] = 4 * 1.5 * np.sin(self.t * np.pi / 8) * x[1] * (0.41 - x[1]) / (0.41**2)
+        return values
+
+# Inlet
+u_inlet = Function(V)
+inlet_velocity = InletVelocity(t)
+u_inlet.interpolate(inlet_velocity)
+bcu_inflow = dirichletbc(u_inlet, locate_dofs_topological(V, fdim, ft.find(inlet_marker)))
+# Walls
+u_nonslip = np.array((0,) * mesh.geometry.dim, dtype=PETSc.ScalarType)
+bcu_walls = dirichletbc(u_nonslip, locate_dofs_topological(V, fdim, ft.find(wall_marker)), V)
+# Obstacle
+bcu_obstacle = dirichletbc(u_nonslip, locate_dofs_topological(V, fdim, ft.find(edge_marker)), V)
+bcu = [bcu_inflow, bcu_obstacle, bcu_walls]
+# Outlet
+bcp_outlet = dirichletbc(PETSc.ScalarType(0), locate_dofs_topological(Q, fdim, ft.find(outlet_marker)), Q)
+bcp = [bcp_outlet]
+
+
+
+def _all_interior_surfaces(x):
+    eps = 1.0e-5
+ 
+    x_min = 0
+    x_max = L
+    y_min = 0
+    y_max = H
+
+    x_mid = np.logical_and(
+        x_min + eps < x[0], x[0] < x_max - eps
+    )
+    y_mid = np.logical_and(
+        y_min + eps < x[1], x[1] < y_max - eps
+    )
+ 
+    all_interior_surfaces = np.logical_and(
+        x_mid, y_mid
+    )
+ 
+    return all_interior_surfaces
+
+facet_dim = 1
+all_interior_facets = locate_entities_boundary(
+    mesh, facet_dim, _all_interior_surfaces
+)
+all_interior_V_dofs = locate_dofs_topological(
+    V, facet_dim, all_interior_facets
+)
+
+# show the interior degrees of freedom around the cylinder
+print(all_interior_V_dofs)
+
+# show that we can shift the cylinder mesh points to the right
+mesh.geometry.x[all_interior_V_dofs,0] += 0.01
+
+# Variational form setup
+
+u = TrialFunction(V)
+v = TestFunction(V)
+u_ = Function(V)
+u_.name = "u"
+u_s = Function(V)
+u_n = Function(V)
+u_n1 = Function(V)
+p = TrialFunction(Q)
+q = TestFunction(Q)
+p_ = Function(Q)
+p_.name = "p"
+phi = Function(Q)
+
+# first step
+f = Constant(mesh, PETSc.ScalarType((0, 0)))
+F1 = rho / k * dot(u - u_n, v) * dx
+F1 += inner(dot(1.5 * u_n - 0.5 * u_n1, 0.5 * nabla_grad(u + u_n)), v) * dx
+F1 += 0.5 * mu * inner(grad(u + u_n), grad(v)) * dx - dot(p_, div(v)) * dx
+F1 += dot(f, v) * dx
+a1 = form(lhs(F1))
+L1 = form(rhs(F1))
+A1 = create_matrix(a1)
+b1 = create_vector(L1)
+
+# second step
+a2 = form(dot(grad(p), grad(q)) * dx)
+L2 = form(-rho / k * dot(div(u_s), q) * dx)
+A2 = assemble_matrix(a2, bcs=bcp)
+A2.assemble()
+b2 = create_vector(L2)
+
+# last step 
+a3 = form(rho * dot(u, v) * dx)
+L3 = form(rho * dot(u_s, v) * dx - k * dot(nabla_grad(phi), v) * dx)
+A3 = assemble_matrix(a3)
+A3.assemble()
+b3 = create_vector(L3)
+
+# setup solvers
+solver1 = PETSc.KSP().create(mesh.comm)
+solver1.setOperators(A1)
+solver1.setType(PETSc.KSP.Type.BCGS)
+pc1 = solver1.getPC()
+pc1.setType(PETSc.PC.Type.JACOBI)
+
+solver2 = PETSc.KSP().create(mesh.comm)
+solver2.setOperators(A2)
+solver2.setType(PETSc.KSP.Type.MINRES)
+pc2 = solver2.getPC()
+pc2.setType(PETSc.PC.Type.HYPRE)
+pc2.setHYPREType("boomeramg")
+
+solver3 = PETSc.KSP().create(mesh.comm)
+solver3.setOperators(A3)
+solver3.setType(PETSc.KSP.Type.CG)
+pc3 = solver3.getPC()
+pc3.setType(PETSc.PC.Type.SOR)
+
+# compute drag and lift coefficients
+n = -FacetNormal(mesh)  # Normal pointing out of obstacle
+dObs = Measure("ds", domain=mesh, subdomain_data=ft, subdomain_id=edge_marker)
+u_t = inner(as_vector((n[1], -n[0])), u_)
+drag = form(2 / 0.1 * (mu / rho * inner(grad(u_t), n) * n[1] - p_ * n[0]) * dObs)
+lift = form(-2 / 0.1 * (mu / rho * inner(grad(u_t), n) * n[0] + p_ * n[1]) * dObs)
+if mesh.comm.rank == 0:
+    C_D = np.zeros(num_steps, dtype=PETSc.ScalarType)
+    C_L = np.zeros(num_steps, dtype=PETSc.ScalarType)
+    t_u = np.zeros(num_steps, dtype=np.float64)
+    t_p = np.zeros(num_steps, dtype=np.float64)
+
+# compute pressures in front of and behind cylinder
+tree = bb_tree(mesh, mesh.geometry.dim)
+points = np.array([[0.15, 0.2, 0], [0.25, 0.2, 0]])
+cell_candidates = compute_collisions_points(tree, points)
+colliding_cells = compute_colliding_cells(mesh, cell_candidates, points)
+front_cells = colliding_cells.links(0)
+back_cells = colliding_cells.links(1)
+if mesh.comm.rank == 0:
+    p_diff = np.zeros(num_steps, dtype=PETSc.ScalarType)
+
+# solve time-dependent problem
+folder = Path("results")
+folder.mkdir(exist_ok=True, parents=True)
+vtx_u = VTXWriter(mesh.comm, "results/dfg2D-3-u.bp", [u_], engine="BP4")
+vtx_p = VTXWriter(mesh.comm, "results/dfg2D-3-p.bp", [p_], engine="BP4")
+xdmf_file = XDMFFile(MPI.COMM_WORLD, "results/velocity.xdmf", "w")
+vtx_u.write(t)
+vtx_p.write(t)
+xdmf_file.write_mesh(mesh)
+xdmf_file.write_function(u_, t)
+
+progress = tqdm.autonotebook.tqdm(desc="Solving PDE", total=num_steps)
+for i in range(num_steps):
+    progress.update(1)
+    # Update current time step
+    t += dt
+    # Update inlet velocity
+    inlet_velocity.t = t
+    u_inlet.interpolate(inlet_velocity)
+
+    # Step 1: Tentative velocity step
+    A1.zeroEntries()
+    assemble_matrix(A1, a1, bcs=bcu)
+    A1.assemble()
+    with b1.localForm() as loc:
+        loc.set(0)
+    assemble_vector(b1, L1)
+    apply_lifting(b1, [a1], [bcu])
+    b1.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    set_bc(b1, bcu)
+    solver1.solve(b1, u_s.vector)
+    u_s.x.scatter_forward()
+
+    # Step 2: Pressure corrrection step
+    with b2.localForm() as loc:
+        loc.set(0)
+    assemble_vector(b2, L2)
+    apply_lifting(b2, [a2], [bcp])
+    b2.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    set_bc(b2, bcp)
+    solver2.solve(b2, phi.vector)
+    phi.x.scatter_forward()
+
+    p_.vector.axpy(1, phi.vector)
+    p_.x.scatter_forward()
+
+    # Step 3: Velocity correction step
+    with b3.localForm() as loc:
+        loc.set(0)
+    assemble_vector(b3, L3)
+    b3.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    solver3.solve(b3, u_.vector)
+    u_.x.scatter_forward()
+
+    # Write solutions to file
+    vtx_u.write(t)
+    vtx_p.write(t)
+    xdmf_file.write_function(u_, t)
+
+    # Update variable with solution form this time step
+    with u_.vector.localForm() as loc_, u_n.vector.localForm() as loc_n, u_n1.vector.localForm() as loc_n1:
+        loc_n.copy(loc_n1)
+        loc_.copy(loc_n)
+
+    # Compute physical quantities
+    # For this to work in paralell, we gather contributions from all processors
+    # to processor zero and sum the contributions.
+    drag_coeff = mesh.comm.gather(assemble_scalar(drag), root=0)
+    lift_coeff = mesh.comm.gather(assemble_scalar(lift), root=0)
+    p_front = None
+    if len(front_cells) > 0:
+        p_front = p_.eval(points[0], front_cells[:1])
+    p_front = mesh.comm.gather(p_front, root=0)
+    p_back = None
+    if len(back_cells) > 0:
+        p_back = p_.eval(points[1], back_cells[:1])
+    p_back = mesh.comm.gather(p_back, root=0)
+    if mesh.comm.rank == 0:
+        t_u[i] = t
+        t_p[i] = t - dt / 2
+        C_D[i] = sum(drag_coeff)
+        C_L[i] = sum(lift_coeff)
+        # Choose first pressure that is found from the different processors
+        for pressure in p_front:
+            if pressure is not None:
+                p_diff[i] = pressure[0]
+                break
+        for pressure in p_back:
+            if pressure is not None:
+                p_diff[i] -= pressure[0]
+                break
+vtx_u.close()
+vtx_p.close()
+xdmf_file.close()
