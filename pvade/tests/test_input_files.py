3c843b010d89e7d7b7c9b02ae4f44af0adb75baf
brookeslawski <brooke.stanislawski@gmail.com>
2024-06-27 16:54:36 -0600

    changes to enable assertion-based pass/fail to reg test

--- pvade/tests/test_input_files.py
+++ pvade/tests/test_input_files.py
@@ -1,5 +1,5 @@
 import pytest
-# import subprocess, sys
+import subprocess
 import os
 import glob
 
@@ -8,28 +8,33 @@ def launch_sim(input_file):
     tf = dt*10 # ten timesteps
     l_char = 0.01
 
-    command = "mpirun -n 8 python ../../ns_main.py --input_file ../../input/" + input_file + " --domain.l_char " + str(l_char) + " --solver.dt " + str(dt) + " --solver.t_final " + str(tf)
-    # command = "mpirun -n 8 python ../../ns_main.py --input_file " + input_file + " --domain.l_char " + str(l_char) + " --solver.dt " + str(dt) + " --solver.t_final " + str(tf)
+    command = f"mpirun -n 2 python ../../ns_main.py --input_file " + input_file + " --domain.l_char " + str(l_char) + " --solver.dt " + str(dt) + " --solver.t_final " + str(tf)
     print(command)
-    os.system(command)
 
-# print(os.path.abspath(os.curdir))
-# print(sys.path[1])
-# files_list = os.listdir("../../input/")
-files_list = glob.glob('../../input/*.yaml')
+    try:
+        tmp = subprocess.check_call(command.split())
+        # os.system(command)
+        return 1 # no errors
+    except: # if any error
+        return 0
+
+# files_list = glob.glob('../../input/*.yaml')
+# files_list = ["../../input/flag2d.yaml"]
+# files_list = ["../../input/flag2d.yaml","../../input/flag2d.yaml"]
+files_list = ["../../input/flag2d.yaml","../../input/2d_cyld.yaml"]
 print(files_list)
-# @pytest.mark.parametrize("input_file", ["flag2d.yaml","2d_cyld.yaml","3d_cyld.yaml",
-#                                         "sim_params.yaml","sim_params_2D.yaml","single_row.yaml"])
-@pytest.mark.parametrize("input_file", ["flag2d.yaml","flag2d.yaml"])
-# @pytest.mark.parametrize("input_file", ["../../input/flag2d.yaml","../../input/2d_cyld.yaml"])
-# @pytest.mark.parametrize("input_file", files_list)
+
+@pytest.mark.parametrize("input_file", files_list)
 def test_launch_with_different_input_files(input_file):
   print("input_file = ",input_file)
-  launch_sim(input_file)
+  result = launch_sim(input_file)
+  print(result)
 
+  assert result == 1
   # not sure the best way to test that this ran - with or without the next line shows "Pass" when there are errors, or just hangs if there is an error
   # assert True # just checks if it runs without errors
 
 # os.system("pwd")
 # test_launch_with_different_input_files(files_list)
-# test_launch_with_different_input_files("flag2d.yaml")
+# test_launch_with_different_input_files("../../input/flag2d.yaml")
+# test_launch_with_different_input_files("../../input/2d_cyld.yaml")
